9a84ba5653a09213cba44fa64d49111635d62766
guitarman@vt.edu <guitarman@vt.edu@3cc41ea0-0e15-11df-9821-6b6fb8eff287>
2010-05-18 12:04:12 +0000

    Moved timemachine into a package under julesverne app; made project run out-of-the-box.

---
+++ trunk/julesverne/timemachine/README.txt
@@ -0,0 +1,4 @@
+timemachine package
+===================
+
+This package can be used by itself in other python (non-Django) projects.
---
+++ trunk/julesverne/timemachine/tests.py
@@ -0,0 +1,7 @@
+import unittest
+from test_dynamic import *
+from test_mixed import *
+from test_static import *
+
+if __name__ == "__main__":
+    unittest.main()
