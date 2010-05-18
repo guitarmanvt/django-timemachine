e1ef783fb202dedbf257e1aee6491a9099c7665f
Baggerone@gmail.com <Baggerone@gmail.com@3cc41ea0-0e15-11df-9821-6b6fb8eff287>
2010-05-18 18:21:23 +0000

    followed change in test_dynamic.py that split it up into a test suite.

--- trunk/julesverne/timemachine/tests.py
+++ trunk/julesverne/timemachine/tests.py
@@ -1,7 +1,8 @@
 import unittest
-from test_dynamic import *
+from test_dynamic import test_main
 from test_mixed import *
 from test_static import *
 
 if __name__ == "__main__":
+    test_main()
     unittest.main()
